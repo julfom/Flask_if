a1be66425b8372e3d5036d741f8a34d287eb6c76
donkeyhot <ifomina@fau.edu>
2024-10-24 01:56:05 +0000

    second push

--- app.py
+++ app.py
@@ -21,6 +21,27 @@ def predict():
     prediction = model.predict(features)[0]
     return render_template('index.html', prediction=round(prediction, 2))
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,12)
+    loaded_model = pickle.load(open("models/linear_regression_mall.sav", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/result',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        
+        if int(result)>50:
+            prediction='High score'
+        else:
+            prediction='Low score'
+            
+        return render_template("result.html",prediction=prediction)
